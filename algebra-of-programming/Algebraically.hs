02c1f4b7339a3d796315ba43b474be954f682d3f
U-CIQDEV\gbazerman <gbazerman@GBAZERMAN-T35.ciqdev.com>
2015-05-19 16:31:15 -0400

    algebraic derivation notes basically complete

--- algebra-of-programming/Algebraically.hs
+++ algebra-of-programming/Algebraically.hs
@@ -4,6 +4,26 @@ module Algebraically where
 import Control.Arrow
 import Data.List (inits, tails)
 
+
+{-
+***
+Notes and Exercises in Algebraic Program Derivation
+Gershom Bazerman, May 2015
+***
+
+Tested with: GHC 7.10, GHC 7.8
+
+The following are a set of notes and exercises to introduce the very basic notions of program derivation in the style of "The Algebra of Programming" (Bird and de Moor, 1997). That book provides the best account of this material and much more. However, there is a long and storied history of work in this field. See for instance:
+
+ "Functional Programming with Bananas, Lenses, Envelopes and Barbed Wire" and follow on work by Meijer, Fokkinga and Paterson (1991)
+"Algorithmics -- Towards programming as a mathematical activity" (Meertens, 1987)
+"An Exploration of the Bird-Meertens Formalism" (Backhouse, 1989)
+"Algebraic Data Types and Program Transformation" (Malcolm, 1990)
+"Upwards and downwards accumulations on trees" (Gibbons, 1992)
+-}
+
+--------------------------------------------------------------
+
 -- Part 1: Fixpoints of Functors, and Algebras on Functors
 
 -- equational reasoning requires equations
@@ -86,18 +106,14 @@ up4 f = listFold f . listMap (fold f)
 -- the universal property shows that this goes the other direction
 
 {-
-
 exercise:
 
 show that up3 and up4 are necessarily the same
-
 -}
 
 {-
-
 exercise:
 fold a = id
-
 -}
 
 {-
@@ -106,7 +122,6 @@ fold fusion:
 h . listFold f = listFold g . listMap h
 ==>
 h . fold f = fold g
-
 -}
 
 ff1 h f g = h . listFold f
@@ -151,16 +166,14 @@ ffex2 = g . listMap (show :: Integer -> String)
           g = undefined -- ?
 
 {-
-
 exercise: show that
 
 listFold a . fold (mkListAlg (listMap (listFold a))) == id
 
 fold (mkListAlg (listMap (listFold a))) . listFold a == id
-
 -}
 
-
+--------------------------------------------------------------
 -- Part 3: The Banana Split Threorem
 
 bs1, bs2, bs3, bs4, bs5, bs6 :: LAlg a b -> LAlg a c -> ListF a [a] -> (b,c)
@@ -195,6 +208,7 @@ exercise, use the banana split theorem to write a single pass average.
 exercise: write the bs theorem as an operation on LAlg explicitly, and argue why it is the same.
 -}
 
+--------------------------------------------------------------
 -- Part 4: Maximum Segment Sum
 -- inspired heavily by http://www.iis.sinica.edu.tw/~scm/2010/maximum-segment-sum-origin-and-derivation/
 
@@ -213,6 +227,16 @@ h [h xs, h ys] == h (xs ++ ys)
 h . concat = h . map h
 
 In such a circumstance, we say h is a "list homomorphism" and it is a special fold that is associative and with a zero element. -- i.e. it is given by the action of a monoid.
+
+Other homomorphism laws:
+
+define m xs ys = h [xs,ys]
+
+now, h = foldr m z . map (\x -> h [x])
+
+(where foldr itself may be executed in parallel as it is a fold over a monoidal operation)
+
+Observe: list homomorphisms are subject to _map_ and _reduce_.
 -}
 
 {-
@@ -241,7 +265,7 @@ scanRight (c,z) = (go,[z])
           go x (y:ys) = c x y : y : ys
 
 {-
-By inspection, tails is initial with regards to scans.
+By inspection, tails is initial with regards to rightward scans.
 
 scanRight alg = map (fold alg) . fold tailsAlg
 
@@ -256,7 +280,7 @@ g = scanRight alg
 and we see
 
 map (fold alg) . listFold tailsAlg :: ListF a [[a]] -> [a]
-listFold (scanRight alg) . listMap (map (fold sumAlg)) :: ListF a [[a]] -> [a]
+listFold (scanRight alg) . listMap (map (fold alg)) :: ListF a [[a]] -> [a]
 
 the two are equal, and therefore by fusion, scanRight alg = map (fold alg) . tailsAlg
 
@@ -266,16 +290,37 @@ scanr f e = map (foldr f e) . tails
 
 -}
 
+{-
+note this is an inefficient inits and scan..
+-}
+
 initsAlg :: LAlg a [[a]]
 initsAlg = (go,[])
-    where go x xs = [x] : map (x:) xs
+    where go x [] = [] : [x] : []
+          go x xs = [] : map (x:) xs
 {-
 *Algebraically> fold initsAlg [1,2,3,4,5]
 [[1],[1,2],[1,2,3],[1,2,3,4],[1,2,3,4,5]]
 -}
 
---TODO inits fusion
--- map (fold alg) . fold initsAlg == ?
+scanLeft :: LAlg a b -> LAlg a [b]
+scanLeft (c,z) = (go,[])
+    where go x [] = [z,c x z]
+          go x ys = z : map (c x) ys
+
+
+{-
+inits fusion
+
+map (fold alg) . fold initsAlg :: ListF a [[a]] -> [a]
+listFold (scanLeft alg) . listMap (map (fold alg)) :: ListF a [[a]] -> [a]
+
+these two are equal, therefore by fusion, scanLeft alg = map (fold alg) . initsAlg
+
+Or:
+
+scanl f e = map (foldl f e) . inits
+-}
 
 -- C) The problem
 
@@ -304,9 +349,129 @@ mss4 = maximum . map (maximum . map sum . inits) . tails
 -- maximum segment sum is the maximum of the prefix sums of the tails
 -- we wish to manipulate mps into a fold, so that we can use scanr fusion.
 
-mps0,mps1 :: [Integer] -> Integer
+mps0,mps1,mps2,mps3,mps4 :: [Integer] -> Integer
 mps0 = maximum . map sum . inits
---inits fusion
-mps1 = maximum . foldr zplus [0]
 
-zplus x xs = 0 : map (x+) xs
+--inits fusion
+
+mps1 = maximum . scanl (+) 0
+
+--expand maximum
+
+mps2 = foldr max 0 . scanl (+) 0
+
+--fold/scan fusion
+
+{-
+I believe the following is correct:
+
+if h and g are list homomorphisms, and
+g x (h y z) = h (g x y) (g x z) -- i.e. h distributes over g
+
+then
+
+foldr h z . scanr g z = foldr (\x y -> h (g x y) z)
+
+If it is not, then it holds in this specific case, and can be carried through by reasoning directly on (+) and max.
+-}
+
+mps3 = foldr (\x y -> max 0 (x + y)) 0
+
+zmax x y = max 0 (x + y)
+
+mps4 = foldr zmax 0
+
+
+mss5,mss6 :: [Integer] -> Integer
+
+--subsitute in the new mps
+mss5 = maximum . map (foldr zmax 0) . tails
+
+-- tails fusion
+mss6 = maximum . scanr zmax 0
+
+-- we've gone from quadratic time to linear time!
+
+{-
+Exercise: observe that while zmax is derived from two list homomorphisms, zmax itself is not a list homomorphism. Argue why.
+-}
+
+--------------------------------------------------------------
+-- Part 5: Parallel Prefix Sum
+
+{-
+Recall that inits as we gave it had complexity O(N^2).
+
+Note that as a foldl, inits may have complexity O(N), and tails does have such complexity as given.
+
+Can we do better?
+
+One school says, obviously not.
+
+Another says, think parallel!
+
+For simplicity we work with suffix sums (generalized scanr), and assume an O(1) list concatenation
+
+-}
+
+{-
+Exercise 1: given an arbitrary operator "op" and an arbitrary z, (scanr op z) is not a list homomorphism. Name a counterexample.
+-}
+
+{-
+Recall: a list homomorphism is a function h and an operator `op` such that:
+
+h xs `op` h ys = h (xs ++ ys) -- i.e. you can combine then translate, or translate than combine.
+
+This induces a monoid with `op` as the action, and h [] as the unit.
+
+Now we wish to define a function of type
+
+scanOp :: (a -> a -> a) -> [a] -> [a] -> [a]
+
+this function obeys the property that:
+
+scanOp op (scanr op z xs) (scanr op z ys) = scanr op z (xs ++ ys)
+
+-- i.e. we want scanOp to be the monoidal operation induced by "scanr op z"
+
+-}
+
+scanOp :: (a -> a -> a) -> [a] -> [a] -> [a]
+-- obvious
+scanOp op xs [] = xs
+scanOp op [] ys = ys
+
+-- scanOp op xs ys = ?
+{-
+
+By the laws of scanr, we know that:
+
+scanr op z (x:xs) = op x (head (scanr op z xs)) : scanr op z xs
+
+We also know that:
+
+scanr op z (xs ++ ys) = _ + scanr op z ys
+
+From this we can conclude that if op is associative,
+
+scanr op z (xs ++ ys) = map (`op` (head (scanr op z ys))) (init $ scanr op z xs) ++ scanr op z ys
+
+From this property we can write our scanOp as desired.
+
+-}
+
+scanOp op xs (y:ys) = map (`op` y) (init xs) ++ (y:ys)
+
+{-
+Checking some cases we see that indeed for (op,z) a monoid algebra:
+
+foldr (scanOp op) [] . map (scanr op z) === scanr op z
+
+and hence scanr may be written as a map reduce, and parallelized with time complexity O(log(N))
+
+See: "Extracting and implementing list homomorphisms in parallel program development" (Gorlatch, 1999).
+
+This includes an application of a generalized method to turn maximum segment sum into a homomorphism as well.
+-}
+
