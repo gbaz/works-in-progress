e5048c9cdd8e8bc3cb4f4abcf8225f668f2ef420
Gershom <gershom@mbp.local>
2016-03-29 11:19:59 -0400

    trying to interp arrows

--- contexts/Lambek.hs
+++ contexts/Lambek.hs
@@ -36,41 +36,36 @@ data Cxt b = CCons (TCart b) (Cxt b) | CNil
 -- todo make simplicial? or make product of categories?
 
 data CxtArr :: Cxt a -> Cxt a -> * where
+  --category
   CXAId  :: CxtArr a a
   CXACompose :: CxtArr b c -> CxtArr a b -> CxtArr a c
+
+  -- that has a terminal object
   CXANil :: CxtArr a CNil
 
+  -- with degeneracy maps (atoms) given by closed terms
   CXAAtom :: CartRepr (Ty a) -> CxtArr cxt (CCons a cxt)
 
+  -- and face maps given by projection
   CXAProj1 :: CxtArr (CCons a cxt) (CCons a CNil)
   CXAProj2 :: CxtArr (CCons a cxt) cxt
 
+  -- with a cartesian structure
   CXAPair  :: CxtArr cxt (CCons a CNil) -> CxtArr cxt (CCons b CNil) -> CxtArr cxt (CCons (TPair a b) CNil)
   CXAPairProj1 :: CxtArr cxt (CCons (TPair a b) CNil) -> CxtArr cxt (CCons a CNil)
   CXAPairProj2 :: CxtArr cxt (CCons (TPair a b) CNil) -> CxtArr cxt (CCons b CNil)
 
-  CXAEval  :: CxtArr (CCons (TPair (TExp a b) a) cxt) (CCons b cxt)
+  -- and a closed structure
+  -- abst aka uncurry
+  CXAAbst :: CxtArr (CCons a cxt) (CCons b CNil) -> CxtArr cxt (CCons (TExp a b) CNil)
+  -- app aka eval
+  CXAApp :: CxtArr cxt (CCons (TExp a b) CNil) -> CxtArr cxt (CCons a CNil) -> CxtArr cxt (CCons b CNil)
 
---  CXAAbs   :: CxtArr (CCons b (CCons a cxt)) (CCons (TExp a b) cxt)
-
---  CXACurry :: CxtArr (CCons a (CCons b cxt)) (CCons (TPair a b) cxt)
---  CXAUncurry :: CxtArr (CCons (TPair a b) cxt) (CCons a (CCons b cxt))
-
-unTerm :: Term cxt a -> CxtArr cxt (CCons a CNil)
-unTerm (ConstTerm x) = x
-unTerm (AppTerm f x) = CXAEval . (CXAPair (unTerm f) (unTerm x))
-unTerm (LamTerm f) = unTerm $ abstTerm (f CXAProj2 (appArrow CXAProj1 $ ConstTerm CXAId))
-
-abstTerm :: Term (CCons a cxt) b -> Term cxt (TExp a b)
-abstTerm = undefined
-
--- tm_mkpair = lamt $ \h x -> lamt $ \g y -> ConstTerm (CXAPair (_ x) (_ y))
--- TODO: Add more constants?
-
+-- Todo add more coherences? normalization?
 cxaCompose :: CxtArr b c -> CxtArr a b -> CxtArr a c
 cxaCompose CXAId f = f
 cxaCompose f CXAId = f
-cxaCompose (CXACompose h g) f = cxaCompose h (cxaCompose g f)
+cxaCompose h (CXACompose g f) = cxaCompose (cxaCompose h g) f
 cxaCompose g f = CXACompose g f
 
 -- todo make instance of classes in categories lib?
@@ -78,23 +73,64 @@ instance Category CxtArr where
     id = CXAId
     (.) = cxaCompose
 
--- terms here are elements or fibers of presheaves. We want the whole thing? Ans -- in all contexts?
+appTm :: Term cxt (TExp a b) -> Term cxt a -> Term cxt b
+appTm f x = ConstTerm (CXAApp (unTerm f) (unTerm x))
 
+unTerm :: Term cxt a -> CxtArr cxt (CCons a CNil)
+unTerm (ConstTerm x) = x
+unTerm (LamTerm f) = CXAAbst . unTerm $ (f CXAProj2 (ConstTerm CXAProj1))
+
+appArrow :: CxtArr c d -> Term d a -> Term c a
+appArrow h t = ConstTerm (unTerm t . h)
+
+tm_mkpair2 :: Term cxt (TExp a (TExp b (TPair a b)))
+tm_mkpair2 = lamt $ \h x -> lamt $ \g y -> ConstTerm (CXAPair (unTerm x . g) (unTerm y))
+
+-- TODO: Add more constants?
+
+
+-- terms here are slices over one element contexts, or exponentials.
+-- TODO: Cata representation of terms?
 data Term cxt a where
     ConstTerm :: CxtArr cxt (CCons a CNil) -> Term cxt a --representable
     LamTerm   :: (forall c. CxtArr c cxt -> Term c a -> Term c b) -> Term cxt (TExp a b)
-    AppTerm   :: Term cxt (TExp a b) -> Term cxt a -> Term cxt b
 
---    AbsTerm   :: Term (CCons a cxt) b -> Term cxt (TExp a b)
---    FooTerm   :: Term (CCons a cxt) b -> Term cxt a -> Term cxt b
+lamt :: (forall c. CxtArr c cxt -> Term c a -> Term c b) -> Term cxt (TExp a b)
+lamt f = ConstTerm . CXAAbst . unTerm $ (f CXAProj2 (ConstTerm CXAProj1))
 
--- TODO: Cata representation of terms?
--- Challenge, move AppTerm, AbsTerm, FooTerm to arrows or derived things..
+--TODO can we write subst?
+-- subst :: Term (CCons a cxt) t -> Term cxt a -> Term cxt t
+-- subst = foo...
 
-appArrow :: CxtArr c d -> Term d a -> Term c a
-appArrow h (ConstTerm g) = ConstTerm $ g . h
-appArrow h (LamTerm f)   = LamTerm $ \g x -> f (h . g) x
-appArrow h (AppTerm g x) = AppTerm (appArrow h g) (appArrow h x)
+
+
+abst :: CartRepr (Ty a) -> Term CNil a
+abst = ConstTerm . CXAAtom
+
+interp :: Term CNil a -> CartRepr (Ty a)
+interp (LamTerm f) = interp . f CXANil . abst
+interp (ConstTerm f) = interpArr f
+
+-- gah, gotta send contexts to concrete reprs, work wholemeal -- send cxtarrs to functions as a whole...
+
+interpArr :: CxtArr CNil (CCons a CNil) -> CartRepr (Ty a)
+interpArr (CXAAtom x) = x
+interpArr (CXAPair x y) = (interpArr x, interpArr y)
+interpArr (CXAPairProj1 x) = fst (interpArr x)
+interpArr (CXAPairProj2 x) = snd (interpArr x)
+interpArr (CXAAbst f) = \x -> interpArr (f . CXAAtom x)
+interpArr (CXAApp f x) = (interpArr f) (interpArr x)
+--interpArr (CXACompose CXAId f) = interpArr f
+interpArr (CXACompose f CXANil) = interpArr f
+interpArr (CXACompose f CXAId) = interpArr f
+interpArr (CXACompose CXAId h@(CXAAtom g)) = undefined --interpArr (CXAAbst f) g
+interpArr (CXACompose (CXAAtom f) g) = undefined
+interpArr (CXACompose f (CXACompose g h)) = interpArr (CXACompose (CXACompose f g) h)
+interpArr (CXACompose (CXACompose _ _) _) = error "welp"
+
+tmInt :: Int -> Term CNil (TBase AInt)
+tmInt i = ConstTerm (CXAAtom i)
+
 
 weakenTerm :: Term cxt a -> Term (CCons b cxt) a
 weakenTerm = appArrow CXAProj2
@@ -105,39 +141,20 @@ liftTm = appArrow CXANil
 varTm :: Term (CCons a CNil) a
 varTm = ConstTerm CXAId
 
---TODO can we write subst?
--- subst :: Term (CCons a cxt) t -> Term cxt a -> Term cxt t
--- subst = foo...
-
--- note that lifting functions puts them in a CNil context on both sides, which ain't useful :-)
-
-
-abst :: CartRepr (Ty a) -> Term CNil a
-abst = ConstTerm . CXAAtom
-
-interp :: Term CNil a -> CartRepr (Ty a)
-interp (LamTerm f) = interp . f CXANil . abst
-interp (ConstTerm (CXAAtom x)) = x
-interp (AppTerm f x) = interp f (interp x)
-interp (ConstTerm (CXACompose f g)) =  interp (appArrow g (ConstTerm f))
-
-tmInt :: Int -> Term CNil (TBase AInt)
-tmInt i = ConstTerm (CXAAtom i)
-
 lam :: (forall c. Term c a -> Term c b) -> Term cxt (TExp a b)
-lam f = LamTerm $ \ h -> f
+lam f = lamt $ \h -> f
+--lam f = LamTerm $ \ h -> f
 
-lamt :: (forall c. CxtArr c cxt -> Term c a -> Term c b) -> Term cxt (TExp a b)
-lamt f = LamTerm f
+tmid = ConstTerm (CXAAbst CXAId . CXANil)
 
 tm_id = lam $ \x -> x
 
 tm_k = lam $ \x -> LamTerm $ \g y -> appArrow g x
 
-tm_s = lamt $ \h f -> lamt $ \h1 g -> lamt $ \h2 x -> AppTerm (AppTerm (appArrow (h1 . h2) f) x) (AppTerm (appArrow h2 g) x)
+tm_s = lamt $ \h f -> lamt $ \h1 g -> lamt $ \h2 x -> appTm (appTm (appArrow (h1 . h2) f) x) (appTm (appArrow h2 g) x)
 
 tm_comp :: Term cxt (TExp (TExp a1 b) (TExp (TExp a a1) (TExp a b)))
-tm_comp = lamt $ \h f -> lamt $ \h1 g -> lamt $ \h2 x -> AppTerm (appArrow (h1 . h2) f) (AppTerm (appArrow h2 g) x)
+tm_comp = lamt $ \h f -> lamt $ \h1 g -> lamt $ \h2 x -> appTm (appArrow (h1 . h2) f) (appTm (appArrow h2 g) x)
 
 
 {-
@@ -159,15 +176,6 @@ appTm (ConstTerm (CXAAtom f)) = abst . f . interp
 
 appTmNil :: Term CNil (TExp a b) -> Term CNil a -> Term CNil b
 appTmNil f = ConstTerm . CXAAtom . interp f . interp
-
--}
-
--- foo = lamt $ \h x -> appArrow CXAEval x
-
-{-
-tm_badcata
-
-tm_s = lam $ \f -> lam $ \g -> lam $ \x -> AppTerm (AppTerm f x) (AppTerm g x)
 
 -}
 
