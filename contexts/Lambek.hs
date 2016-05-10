d4af55a0c8664963847de378c4cb80adb24d72d5
U-CIQDEV\gbazerman <gbazerman@GBAZERMAN-T35.ciqdev.com>
2016-05-10 18:33:54 -0400

    cleanup and documentation

--- contexts/Lambek.hs
+++ contexts/Lambek.hs
@@ -13,11 +13,16 @@ import Control.Concurrent
 Explorations in lambda terms as elements of presheaves, or generally as slices in a category of contexts.
 -}
 
+-- We begin with objects of cartesian closed categories over some base index of types.
 data TCart b = TUnit | TPair (TCart b) (TCart b) | TExp (TCart b) (TCart b) | TBase b
 
+-- Base indices are mapped to types via Repr
 type family Repr a :: *
+
+-- Cartesian objects over the base are mapped to types via CartRepr
 type family CartRepr a :: *
 
+-- Ty is used to wrap polykinded things up in kind *
 data Ty a
 
 type instance CartRepr (Ty (TBase a)) = Repr (Ty a)
@@ -31,37 +36,49 @@ type instance Repr (Ty AInt) = Int
 type instance Repr (Ty AString) = String
 type instance Repr (Ty ADouble) = Double
 
+
+-- A Context b is a list of cartesian objects over base index b
 data Cxt b = CCons (TCart b) (Cxt b) | CNil
 
+
+-- We define a category of contexts by providing the morphisms as a GADT
+-- These morphisms can also be read in logical terms:
+--
 -- CxtArr a b is a judgment a |- b
 -- CxtArr a b -> CxtArr c d is an inference rule
 --   a |- b
 -- ---------
 --   c |- d
--- Cube?
 
--- todo make simplicial? or make product of categories?
+-- This category also can be read topologically as having a simplicial structure induced by face and degeneracy maps.
 
 data CxtArr :: Cxt a -> Cxt a -> *  where
+  -- To be a category we must have id and composition
   CXAId  :: CxtArr a a
   CXACompose :: CxtArr b c -> CxtArr a b -> CxtArr a c
+
+  -- We have a terminal object
   CXANil :: CxtArr a CNil
 
-  CXAAtom :: CartRepr (Ty a) -> CxtArr cxt (CCons a cxt)
-
+  -- We have face maps
   CXAWeaken :: CxtArr (CCons a cxt) cxt
 
-  CXAEval  :: CxtArr (CCons (TPair (TExp a b) a) cxt) (CCons b cxt)
+  -- We have degeneracy maps given by every inhabitant of our underlying terms
+  CXAAtom :: CartRepr (Ty a) -> CxtArr cxt (CCons a cxt)
 
-  CXAAbs   :: CxtArr (CCons a cxt) (CCons b CNil) ->  CxtArr cxt (CCons (TExp a b) CNil)
-  CXALam    :: (forall c. CxtArr c cxt -> CxtArr c (CCons a c2) -> CxtArr c (CCons b c2)) -> CxtArr cxt (CCons (TExp a b) c2)
-
-  -- we don't really need these?
+  -- We also have a cartesian structure
   CXAPair  :: CxtArr cxt (CCons a c2) -> CxtArr cxt (CCons b c2) -> CxtArr cxt (CCons (TPair a b) c2)
   CXAPairProj1 :: CxtArr (CCons (TPair a b) cxt) (CCons a cxt)
   CXAPairProj2 :: CxtArr (CCons (TPair a b) cxt) (CCons b cxt)
 
+  -- And a closed structure (aka uncurry and eval)
+  CXAEval  :: CxtArr (CCons (TPair (TExp a b) a) cxt) (CCons b cxt)
+  CXAAbs   :: CxtArr (CCons a cxt) (CCons b c) -> CxtArr cxt (CCons (TExp a b) c)
 
+  -- We also add directly the internal hom in a slice category, which _also_ should give a closed structure.
+  CXALam   :: (forall c. CxtArr c cxt -> CxtArr c (CCons a c2) -> CxtArr c (CCons b c2)) -> CxtArr cxt (CCons (TExp a b) c2)
+
+-- A Hacky Show Instance
 instance Show (CxtArr a b) where
     show CXAId = "CXAId"
     show (CXACompose g f) = show g ++ " . " ++ show f
@@ -69,75 +86,101 @@ instance Show (CxtArr a b) where
     show (CXAAtom x) = "CXAAtom"
     show CXAWeaken = "CXAWeaken"
     show CXAEval = "CXAEval"
+    show (CXALam f) = "CXALam"
     show (CXAAbs f) = "CXAAbs (" ++ show f ++ ")"
-    show (CXALam f) = "CXALam" -- ++ (show (f CXAWeaken CXAId)) ?
     show (CXAPair f g) = "(" ++ show f ++ ", " ++ show g ++ ")"
     show CXAPairProj1 = "CXAPairProj1"
     show CXAPairProj2 = "CXAPairProj2"
 
+-- We give axioms on our category as conditions on coherence of composition
 -- As per section 10 of  http://arxiv.org/pdf/math/9911073.pdf
-
 cxaCompose :: CxtArr b c -> CxtArr a b -> CxtArr a c
 cxaCompose CXAId f = f
 cxaCompose f CXAId = f
-cxaCompose h (CXACompose g f) = CXACompose (cxaCompose h g) f
-cxaCompose CXAEval (CXAPair (CXACompose (CXAAbs f) CXAWeaken) CXAId) = f
---cxaCompose CXAEval (CXAPair (CXACompose (CXALam f) CXAWeaken) CXAId) = f CXAWeaken CXAId
 cxaCompose CXAEval (CXAPair (CXALam f) g) = f CXAId g
 cxaCompose CXAPairProj1 (CXAPair a b) = a
 cxaCompose CXAPairProj2 (CXAPair a b) = b
 
---TODO fix all the rules in composition... maybe move abs to deal with pairs somehow?
-cxaCompose g f = CXACompose g f
+-- If the axioms are right, we should never need CXACompose directly, i.e. never get stuck.
+-- cxaCompose h (CXACompose g f) = CXACompose (cxaCompose h g) f
+-- cxaCompose CXAEval (CXAPair (CXACompose (CXAAbs f) CXAWeaken) CXAId) = f
+-- cxaCompose g f = CXACompose g f
 
--- TODO write cxaPair as a "smart constructor"
-
--- todo make instance of classes in categories lib?
+-- CxtArr indeed gives a category
 instance Category CxtArr where
     id = CXAId
     (.) = cxaCompose
 
--- terms here are elements or fibers of presheaves.
-newtype Term cxt a = Term (CxtArr cxt (CCons a CNil)) deriving Show
+-- Terms here are elements or fibers of presheaves. Alternately, terms of type A are elements of the slice category over the context containing only A
+data Term cxt a where
+    Term :: CxtArr cxt (CCons a CNil) -> Term cxt a
+-- An experiment in progress
+--    LamTerm :: (forall c. CxtArr c cxt -> CxtArr c (CCons d cxt) -> CxtArr c (CCons b cxt)) -> Term cxt (TExp d b)
+
+instance Show (Term cxt a) where
+    show (Term x) = show x
+--    show (LamTerm f) = "LamTerm"
 
 unTerm :: Term cxt a -> CxtArr cxt (CCons a CNil)
 unTerm (Term x) = x
+-- unTerm (LamTerm f) = CXAAbs (f CXAWeaken _)
+
+-- We can now write lam, app etc over terms just as we would in a typical embedding
 
 lamTerm :: (forall c. CxtArr c cxt -> Term c a -> Term c b) -> Term cxt (TExp a b)
+--lamTerm f = LamTerm (\m x -> unTerm (f m (Term x)))
 lamTerm f = Term (CXALam (\m x -> unTerm (f m (Term x))))
 
 appTerm :: Term cxt (TExp a b) -> Term cxt a -> Term cxt b
 appTerm f x = Term (CXAEval . (CXAPair (unTerm f) (unTerm x)))
 
-appArrow :: CxtArr c d -> Term d a -> Term c a
-appArrow h (Term g) = Term $ g . h
-
-interp' :: Term CNil a -> CartRepr (Ty a)
-interp' (Term (CXAAtom x)) = x
-interp' (Term (CXACompose f g)) =  interp (appArrow g (Term f)) -- can we just make composition?
-interp' (Term (CXALam f)) = interp . Term . f CXAId . CXAAtom
-interp' (Term (CXAAbs f)) = interp (Term (CXALam $ \_ x -> f . x))
-interp' (Term (CXAPair f g)) = (interp (Term f), interp (Term g))
-
-interp :: Term CNil a -> CartRepr (Ty a)
-interp x = unsafePerformIO (threadDelay 100000) `seq` traceShow x (interp' x)
-
 weakenTerm :: Term cxt a -> Term (CCons b cxt) a
-weakenTerm = appArrow CXAWeaken
+weakenTerm = Term . (. CXAWeaken) . unTerm
 
 liftTm :: Term CNil a -> Term cxt a
-liftTm = appArrow CXANil
+liftTm = Term . (. CXANil) . unTerm
 
 varTm :: Term (CCons a CNil) a
 varTm = Term CXAId
 
---TODO can we write subst?
--- subst :: Term (CCons a cxt) t -> Term cxt a -> Term cxt t
--- subst = foo...
+appArrow :: CxtArr c d -> Term d a -> Term c a
+appArrow h (Term g) = Term $ g . h
 
+
+-- Abstraction (in the sense of nbe) is a free operation
 abst :: CartRepr (Ty a) -> Term CNil a
 abst = Term . CXAAtom
 
+-- Interpretation does the obvious thing
+interp' :: Term CNil a -> CartRepr (Ty a)
+interp' (Term (CXAAtom x)) = x
+interp' (Term (CXAPair f g)) = (interp (Term f), interp (Term g))
+interp' (Term (CXALam f)) = interp . Term . f CXAId . unTerm . abst
+interp' (Term (CXAAbs f)) = interp (Term (CXALam $ \_ x -> f . x))
+
+--Again we should never need explicit composition
+--interp' (Term (CXACompose f g)) =  interp (Term (f . g))
+
+-- Here again is a partial experiment
+--interp' (LamTerm f) = interp . Term . f CXAId . CXAAtom
+--interp' (Term (CXAAbs f)) = interp (LamTerm $ \_ x -> f . x)
+
+-- Here we wrap interpretation for tracing evaluation
+interp :: Term CNil a -> CartRepr (Ty a)
+interp x = unsafePerformIO (threadDelay 100000) `seq` traceShow x (interp' x)
+
+-- And nbe does what we want
+nbe :: Term CNil a -> Term CNil a
+nbe = abst . interp
+
+-- To be done -- formulate terms as a module over a (relative) monad.
+-- To be done -- write substitution on terms directly.
+--               subst :: Term (CCons a cxt) t -> Term cxt a -> Term cxt t
+
+-- To be done, explore if LamTerm gives proper derivability vs. admissibility rule
+
+
+-- Some examples
 tmInt :: Int -> Term CNil (TBase AInt)
 tmInt i = Term (CXAAtom i)
 
@@ -155,7 +198,6 @@ tm_k = lam $ \x -> lamTerm $ \g y -> appArrow g x
 
 tm_s = lamt $ \h f -> lamt $ \h1 g -> lamt $ \h2 x -> appTerm (appTerm (appArrow (h1 . h2) f) x) (appTerm (appArrow h2 g) x)
 
-
 tm_comp :: Term cxt (TExp (TExp a1 b) (TExp (TExp a a1) (TExp a b)))
 tm_comp = lamt $ \h f -> lamt $ \h1 g -> lamt $ \h2 x -> appTerm (appArrow (h1 . h2) f) (appTerm (appArrow h2 g) x)
 
@@ -163,113 +205,9 @@ tm_f :: Term CNil (TExp (TExp a b) (TExp a a))
 tm_f = appTerm tm_s tm_k
 
 
--- this works
+-- Testing the interpreter
 test = (interp (tm_id :: Term cxt (TExp (TBase AInt) (TBase AInt)))) $ 12
+test2 = (interp (tm_id2 :: Term CNil (TExp (TBase AInt) (TBase AInt)))) $ 12
 
 itm_f :: (Double -> String) -> (Double -> Double)
 itm_f = interp (tm_f :: Term CNil (TExp (TExp (TBase ADouble) (TBase AString)) (TExp (TBase ADouble) (TBase ADouble))))
-
-
-
-
-{-
-unTerm :: Term cxt a -> CxtArr cxt (CCons a CNil)
-unTerm (ConstTerm x) = x
-unTerm (AppTerm f x) = CXAEval . (CXAPair (unTerm f) (unTerm x))
-unTerm (LamTerm f) = unTerm $ abstTerm (f CXAProj2 (appArrow CXAProj1 $ ConstTerm CXAId))
-
-abstTerm :: Term (CCons a cxt) b -> Term cxt (TExp a b)
-abstTerm = undefined
--}
-
-{-
-tm_mkpair :: Term cxt (TExp a (TExp b (TPair b a)))
-tm_mkpair = lamt $ \h x -> lamt $ \g y -> (FooTerm (FooTerm (ConstTerm (CXAProj1 . CXACurry))
-                                                                (appArrow CXAProj2 y)) (appArrow g x))
--}
-
-
-{-
-
-interpGen :: (forall c. CxtArr c cxt) -> Term cxt a -> CartRepr (Ty a)
-interpGen h (LamTerm f) = interpGen h . f h . liftTm . abst
-
-
-appTm :: Term cxt (TExp a b) -> Term cxt a -> Term cxt b
-appTm (LamTerm f) = (f CXAId)
-appTm (ConstTerm (CXAAtom f)) = abst . f . interp
-
-appTmNil :: Term CNil (TExp a b) -> Term CNil a -> Term CNil b
-appTmNil f = ConstTerm . CXAAtom . interp f . interp
-
--}
-
--- foo = lamt $ \h x -> appArrow CXAEval x
-
-{-
-tm_badcata
-
-tm_s = lam $ \f -> lam $ \g -> lam $ \x -> AppTerm (AppTerm f x) (AppTerm g x)
-
--}
-
-{-
-tmBadCase :: Term cxt (TExp (TBase AInt) (TBase AInt))
-tmBadCase = LamTerm $ \ x -> case x of (ConstTerm _) -> tmInt 24; _ -> x
--}
-{-
-
-type family ToCxt a :: Cxt b where
-    ToCxt TUnit = CNil
-    ToCxt (TPair a b) = CCons a (ToCxt b)
-    ToCxt f = CCons f CNil
-
-type family ToCart a :: TCart b where
-    ToCart CNil = TUnit
-    ToCart (CCons a cxt) = TPair a (ToCart cxt)
-
--- as per section 10 of Dosen and Petric, http://arxiv.org/pdf/math/9911073.pdf
-
-data CartArrow :: TCart k -> TCart k -> * where
-    CAProj1 :: CartArrow (TPair a b) a
-    CAProj2 :: CartArrow (TPair a b) b
-    CAConst :: CartArrow a TUnit
-    CACompose :: CartArrow b c -> CartArrow a b -> CartArrow a c
-    CAPair :: CartArrow c a -> CartArrow c b -> CartArrow c (TPair a b)
-    CAId :: CartArrow a a
-
-    CAAtom :: (Show (Repr (Ty b))) => Repr (Ty b) -> CartArrow TUnit (TBase b)
-
---    CAUncurry :: CartArrow (TPair c a) b -> CartArrow c (TExp a b)
---    CAEval  :: CartArrow (TPair (TExp a b) a) b
-
-
-deriving instance Show (CartArrow a b)
-
-caCompose :: CartArrow b c -> CartArrow a b -> CartArrow a c
-caCompose CAId f = f
-caCompose f CAId = f
-caCompose (CACompose h g) f = caCompose h (caCompose g f)
---caCompose CAEval (CAPair (CACompose (CAUncurry f) CAProj1) CAProj2) = f
-caCompose g f = CACompose g f
-
---caUncurry :: CartArrow (TPair c a) b -> CartArrow c (TExp a b)
---caUncurry (CACompose CAEval (CAPair (CACompose g CAProj1) CAProj2)) = g
---caUncurry f = CAUncurry f
-
--- TODO caPair which requires Eq
-
-instance Category CartArrow where
-    id = CAId
-    (.) = caCompose
-
-
-data Op c a b = Op (c b a)
-
-instance Category c => Category (Op c) where
-    id = C.id
-    (Op g) . (Op f) = Op (f . g)
-
-type CArr = Op CxtArr
-
--}
