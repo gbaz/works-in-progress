4a4701caee64d16ba4e4b4f7a16b99d041503549
Gershom Bazerman <gershom@arista.com>
2022-04-25 15:57:41 -0400

    fixup

--- infinitraverse/IT.hs
+++ infinitraverse/IT.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveFunctor, StandaloneDeriving, UndecidableInstances, PolyKinds, MultiParamTypeClasses, FlexibleInstances #-}
+{-# LANGUAGE DeriveFunctor, StandaloneDeriving, UndecidableInstances, PolyKinds, MultiParamTypeClasses, FlexibleInstances, TypeFamilies, RankNTypes, TupleSections #-}
 
 module IT where
 import Control.Monad.Identity
@@ -38,9 +38,177 @@ instance Applicative Later where
   Later f <*> Later x = Later (f x)
 
 lfix :: (Later a -> a) -> a
-lfix f = let x = f (pure x) in x
+lfix f = fix (f . Later) -- let x = f (pure x) in x
 
 data Stream a = Nil | Cons a (Later (Stream a)) deriving (Functor, Show)
+
+sinterleave :: Stream a -> Stream a -> Stream a
+sinterleave = lfix $ \f s1 s2 -> case s1 of
+  (Cons x xs) -> Cons x (f <*> pure s2 <*> xs)
+  _ -> s2
+
+szip :: Stream a -> Stream b -> Stream (a, b)
+szip = lfix $ \f s1 s2 -> case (s1, s2) of
+  (Cons x xs, Cons y ys) -> Cons (x,y) (f <*> xs <*> ys)
+  _ -> Nil
+
+class EvalLater a where
+  type Result a
+  leval :: a -> Result a
+
+instance EvalLater a => EvalLater (Later a) where
+  type Result (Later a) = Result a
+  leval (Later a) = leval a
+
+instance EvalLater Int where
+  type Result Int = Int
+  leval = id
+
+{-
+instance EvalLater a => EvalLater [a] where
+  type Result [a] = [Result a]
+  leval = fmap leval
+-}
+
+instance (EvalLater a, EvalLater b) => EvalLater (a, b) where
+  type Result (a, b) = (Result a, Result b)
+  leval (x, y) = (leval x, leval y)
+
+instance EvalLater a => EvalLater (Stream a) where
+  type Result (Stream a) = [Result a]
+  leval Nil = []
+  leval (Cons x xs) = leval x : leval xs
+
+data Delay a = Now a | Wait (Later (Delay a)) deriving (Show, Functor)
+
+instance Applicative Delay where
+
+slast :: Stream a -> Delay (Maybe a)
+slast = go Nothing
+  where go = lfix $ \f def s1 ->
+         case s1 of
+          (Cons x xs) -> Wait $ f <*> pure (Just x) <*> xs
+          Nil -> Now def
+
+
+data PStream a =
+      PNil
+    | PWait (Later (PStream a))
+    | PCons a (PStream a)
+
+instance EvalLater a => EvalLater (Delay a) where
+  type Result (Delay a) = Result a
+  leval (Now x) = leval x
+  leval (Wait x) = leval x
+
+instance EvalLater a => EvalLater (PStream a) where
+   type Result (PStream a) = [Result a]
+   leval PNil = []
+   leval (PWait x) = leval x
+   leval (PCons x xs) = leval x : leval xs
+
+class EvalLater a => LiftLater a where
+  llift :: Result a -> a
+{-
+instance LiftLater a => LiftLater [a] where
+  llift = fmap llift
+-}
+instance (LiftLater a, EvalLater b) => EvalLater (a -> b) where
+  type Result (a -> b) = Result a -> Result b
+  leval f = leval . f . llift
+
+instance (LiftLater a, LiftLater b) => LiftLater (a -> b) where
+  llift f = llift . f . leval
+
+instance (LiftLater a) => LiftLater (Later a) where
+  llift = Later . llift
+
+instance (LiftLater a, LiftLater b) => LiftLater (a, b) where
+  llift (x, y) = (llift x, llift y)
+
+instance LiftLater Int where
+  llift = id
+
+instance LiftLater a => LiftLater (Delay a) where
+  llift = Now . llift
+
+{-
+predict' :: (Applicative f, Predict f, LiftLater (f a)) => Later (f a) -> f (Later a)
+predict' x =
+  let s = ssequence (Cons (pure undefined) (fmap (\z -> Cons z (Later Nil)) x))
+  in fmap (\z -> case z of (Cons _ q) -> fmap (\r -> case r of (Cons t _) -> t) q) s
+-}
+
+sequenceL :: Applicative f => [f a] -> f [a]
+sequenceL = fix $ \rec x -> case x of
+  [] -> pure []
+  (x:xs) -> (:) <$> x <*> rec xs
+
+sequenceS :: (Applicative f, Predict f) => Stream (f a) -> f (Stream a)
+sequenceS = lfix $ \rec x -> case x of
+     Nil -> pure Nil
+     Cons a s ->
+         Cons
+           <$> a
+           <*> predict (rec <*> s)
+
+sequenceS' :: (a ~ Result a, EvalLater a, Applicative f, Predict f) => Stream (f a) -> f [a]
+sequenceS' z = fmap leval $ (fix $ \rec x -> case x of
+     Nil -> pure Nil
+     Cons a s ->
+         Cons
+           <$> a
+           <*> predict (rec <$> s)) z
+
+
+sequenceS'' :: (LiftLater (Stream a), a ~ Result a, EvalLater a, Applicative f, Predict f) => Stream (f a) -> f [a]
+sequenceS'' z = (fix $ \rec x -> fmap leval $ case x of -- first outside then inside
+     Nil -> pure Nil
+     Cons a s ->
+         Cons
+           <$> a
+           <*> predict (fmap llift . rec <$> s)) z
+
+sequenceS''' :: (LiftLater (Stream a), a ~ Result a, Result (f [a]) ~ f [a], EvalLater (f [a]), EvalLater a, Applicative f, Predict f) => Stream (f a) -> f [a]
+sequenceS''' = fix $ \rec x -> case x of -- first outside then inside
+     Nil -> pure []
+     Cons a s ->
+         (:)
+           <$> a
+           <*> leval (rec <$> s)
+
+
+sequenceS4 :: (Applicative f, EvalLater (f [a]), Result (f [a]) ~ f [a]) => Stream (f a) -> f [a]
+sequenceS4 = fix $ \rec x -> case x of -- first outside then inside
+     Nil -> pure []
+     Cons a s ->
+         (:)
+           <$> a
+           <*> leval (rec <$> s)
+
+{-
+sequenceS' :: (Applicative f, Predict f) => Stream (f Int) -> f [Int]
+sequenceS' = fix $ \rec x -> case x of
+     Nil -> pure []
+     Cons a s ->
+         (:)
+           <$> a
+           <*> fmap leval (predict (rec <$> s))
+-}
+{-
+predict' x =
+  let s = (lsl) (Cons (pure undefined) (fmap (\z -> Cons z (Later Nil)) x))
+  in fmap (\z -> case z of (Cons _ q) -> fmap (\r -> case r of (Cons t _) -> t) q) s
+-}
+{-
+sequenceS = fix $ \rec x -> case x of
+     Nil -> pure Nil
+     Cons a s -> Cons <$> a <*> predict ((rec . Later) <*> s)
+-}
+
+-- predictCont :: Later ((a -> Int) -> Int) -> (Later a -> Int) -> Int
+-- predictCont x = \k -> _ (x <*> _ k)
+
 
 -- not really a monoid at all, its a tensor over later?? also we have Stream (Later a) -> Later (Stream a)
 -- so its f a -> f (Later a) -> f a
@@ -77,7 +245,7 @@ class Predict f where
   -- can't give check :: f (Later a) -> Later (f a) because of reader.
   -- this really blows up in the case of the update monad.
   -- we can factorize out the delay in the state, but that's sort of uninteresting
-  delay :: f a -> f a
+  -- delay :: f a -> f a
 
 -- we need an evaluate morphism :: f a -> Eventually (f a) and the property that delay . evaluate = id. delay should be uniquely defined by the predict . pure equation.
 
@@ -117,20 +285,28 @@ foo f = (\t f x -> if x then t else f) <$> (f True) <*> (f False)
 
 instance Predict Later where
   predict = id
-  delay = id
 
 instance Predict Identity where
   predict = Identity . fmap runIdentity
-  delay = id
 
 instance Predict (Reader r) where
   predict x = reader $ \r -> fmap (($ r) . runReader) x
-  delay = id
 
 instance Monoid w => Predict (Writer (Stream w)) where
   predict x = writer $ (fst . runWriter <$> x, shiftStream $ snd . runWriter <$> x)
-  delay x = case runWriter x of
-     (x, y) -> writer (x, shiftStream $ pure y)
+
+
+instance Monoid w => Monoid (Delay w) where
+instance Semigroup w => Semigroup (Delay w) where
+
+instance Monoid w => Predict (Writer (Delay w)) where
+  predict x = writer $ (fst . runWriter <$> x, Wait $ snd . runWriter <$> x)
+
+
+instance Monoid (PStream a)
+instance Semigroup (PStream a)
+instance Predict (Writer (PStream a)) where
+  predict x = writer $ (fst . runWriter <$> x, PWait $ snd . runWriter <$> x)
 
 class Sequence t where
   ssequence :: (Applicative f, Predict f) => t (f a) -> f (t a)
@@ -149,7 +325,6 @@ instance Sequence Tree where
 
 instance (Functor f, Predict f, Predict g) => Predict (Compose f g) where
   predict = Compose . fmap predict . predict . fmap getCompose
-  delay (Compose x) = Compose (fmap delay . delay $ x)
 
 data Update s a = Update {runUpdate :: (Stream s -> (Stream s,a))} deriving Functor -- stream of s to single s
 
@@ -163,7 +338,6 @@ instance Monoid s => Monad (Update s) where
 instance Monoid s => Predict (Update s) where
   predict x = Update $ \s -> let rxs = fmap (($ s) . runUpdate) x in
    (shiftStream (fmap fst rxs), fmap snd rxs)
-  delay x = Update $ \s -> let (a, b) = runUpdate x s in (shiftStream . pure $ a, b)
 
 
 
@@ -190,12 +364,16 @@ getU f = Update $ \s -> (Nil, f s)
 
 goU (Update f) = f Nil
 
+instance Predict (c,) where
 
 instance Monoid s => Predict (State (Stream s)) where
   predict x = state $ \s -> let rs = fmap (`runState` s) x
                             in (fmap fst rs, shiftStream (fmap snd rs))
-  delay x = state $ \s -> let rs = runState x s
-                            in (fst rs, shiftStream . pure . snd $ rs)
+
+instance Predict (State s) where
+  predict x = state $ \s -> let rs = fmap (`runState` s) x
+                            in (fmap fst rs, undefined (fmap snd rs))
+
 
 one :: a -> Stream a
 one x = Cons x $ Later Nil
@@ -240,7 +418,6 @@ instance Predict Stream where
            headPartial _ = error "headPartial" -- works for infinite streams but not normal streams.
            tailPartial (Cons x xs) = xs
            tailPartial Nil = error "nil"
-  delay = id
 
 tfS = listToStream [True, False]
 
